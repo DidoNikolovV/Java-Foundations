c85d7433a771a6ecf476528c1b3ceeb44e213cef
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-09-27 08:43:34 +0300

    SpecialNumbers

---
+++ data-types-variables/src/SpecialNumbers.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class SpecialNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+
+        int n = Integer.parseInt(sc.nextLine());
+
+        for(int i = 1; i <= n; i++) {
+            int number = i;
+            int sum = 0;
+            while(number > 0) {
+                int digit = number % 10;
+                sum += digit;
+                number /=10;
+            }
+
+            boolean isSpecial = false;
+
+            if(sum == 5 || sum == 7 || sum == 11) {
+                isSpecial = true;
+            }
+
+            if(isSpecial) {
+                System.out.printf("%d -> True\n", i);
+            } else {
+                System.out.printf("%d -> False\n", i);
+            }
+        }
+    }
+}
