9f2f466136e330b4bef4a9b3f1aeb5e1a275368e
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-10-17 08:56:10 +0300

    Car solution with constructors

--- object-and-classes/src/Car.java
+++ object-and-classes/src/Car.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
 
 public class Car {
-    String brand;
-    String model;
-    int horsePower;
+    private String brand;
+    private String model;
+    private int horsePower;
+
+    public Car() {
+    }
 
     public Car(String brand) {
         this.brand = brand;
@@ -20,53 +23,16 @@ public class Car {
         this.horsePower = horsePower;
     }
 
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public int getHorsePower() {
-        return horsePower;
-    }
-
-    public void setHorsePower(int horsePower) {
-        this.horsePower = horsePower;
-    }
-
     @Override
     public String toString() {
-        return String.format("%s:%s:%d", this.brand, this.model, this.horsePower);
+        if(horsePower == -1) {
+            return this.brand + ":" + this.model;
+        }
+        return this.brand + ":" + this.model + ":" + this.horsePower;
     }
 
     public static void main(String[] args) {
-
-        var car = new Car("Toyota", "Yaris", 110);
-        System.out.println(car);
-
-        Scanner scanner = new Scanner(System.in);
-
-        String brand = scanner.nextLine();
-        String model = scanner.nextLine();
-        int horsePower = scanner.nextInt();
-
-//        Car car1 = new Car(brand);
-//        Car car2 = new Car(brand, model);
-//        car.setBrand(brand);
-//        car.setModel(model);
-//        car.setHorsePower(horsePower);
-
-        System.out.println(car.toString());
-
+        Car c = new Car("Toyota", "Yaris", 110);
+        System.out.println(c);
     }
 }
